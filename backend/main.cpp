d70a185e9143eb4a6ca4db4e96a732f58886afb3
dzen03 <43926347+dzen03@users.noreply.github.com>
2024-04-12 17:45:26 +0300

    write http server and add mock data

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,3 +1,15 @@
+#include "Server.h"
+
+using namespace simple_http_server;
+
 int main() {
+  Server server;
+
+  server.MapUrl("/", [](const Server::Request&){
+    return Server::CreateResponse(200, R"([{"id":11,"from":{"x":2,"y":3},"to":{"x":2,"y":2}},{"id":15,"from":{"x":3,"y":3},"to":{"x":2,"y":3}}])");
+  });
+
+  server.Start();
+
   return 0;
 }
