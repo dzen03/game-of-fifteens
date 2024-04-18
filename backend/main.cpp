aa4319b14e5e443a3e27d0079ecf3fcbf2580310
dzen03 <43926347+dzen03@users.noreply.github.com>
2024-04-18 20:44:32 +0300

    minor back change

--- backend/main.cpp
+++ backend/main.cpp
@@ -5,11 +5,16 @@ using namespace simple_http_server;
 int main() {
   Server server;
 
-    server.MapUrl("/", [](const Server::Request&){
-        return Server::CreateResponse(200, R"([{"id":11,"from":{"x":2,"y":3},"to":{"x":2,"y":2}},{"id":15,"from":{"x":3,"y":3},"to":{"x":2,"y":3}}])", "", {{"Access-Control-Allow-Origin", "*"}});
-    });
+  server.MapUrl("/solve", [](const Server::Request&){
+    // mock up response
+    return Server::CreateResponse(200,
+                                  R"([{"id":11,"from":{"x":2,"y":3},"to":{"x":2,"y":2}},)"
+                                  R"({"id":15,"from":{"x":3,"y":3},"to":{"x":2,"y":3}}])",
+                                  "",
+                                  {{"Access-Control-Allow-Origin", "*"}});
+  });
 
-  server.Start();
+  server.Start("0.0.0.0", 8080);
 
   return 0;
 }
