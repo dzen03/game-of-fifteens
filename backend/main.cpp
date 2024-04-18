c1027089e6b349225bfc2d19059055cb872dd4d5
nekitisov <nekitisov74@gmail.com>
2024-04-18 20:39:08 +0300

    add ajax async request and dynamic solution

--- backend/main.cpp
+++ backend/main.cpp
@@ -5,9 +5,9 @@ using namespace simple_http_server;
 int main() {
   Server server;
 
-  server.MapUrl("/", [](const Server::Request&){
-    return Server::CreateResponse(200, R"([{"id":11,"from":{"x":2,"y":3},"to":{"x":2,"y":2}},{"id":15,"from":{"x":3,"y":3},"to":{"x":2,"y":3}}])");
-  });
+    server.MapUrl("/", [](const Server::Request&){
+        return Server::CreateResponse(200, R"([{"id":11,"from":{"x":2,"y":3},"to":{"x":2,"y":2}},{"id":15,"from":{"x":3,"y":3},"to":{"x":2,"y":3}}])", "", {{"Access-Control-Allow-Origin", "*"}});
+    });
 
   server.Start();
 
